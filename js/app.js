051f0bc7a71443864d4181770e05c37f3391e86d
mike v <xvx1138@protonmail.com>
2019-03-05 20:15:58 +0100

    initial commit

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-// stuff
+//  back to top button
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
